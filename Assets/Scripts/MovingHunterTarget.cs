63938f8c50a0d7c514271c3daaa1437df9f9d801
Amos <amos.koh.tk@gmail.com>
2023-02-06 18:19:09 +0000

    update moving hunter target script

--- Assets/Scripts/MovingHunterTarget.cs
+++ Assets/Scripts/MovingHunterTarget.cs
@@ -94,9 +94,17 @@ public class MovingHunterTarget : MonoBehaviour
         }
     }
 
+    /* private void SetNewTargetPosition()
+     {
+         Vector2 canvasSize = new Vector2(canvasRectTransform.rect.width, canvasRectTransform.rect.height) / 2f;
+         float x = Random.Range(-canvasSize.x + rectTransform.sizeDelta.x / 2, canvasSize.x - rectTransform.sizeDelta.x / 2);
+         float y = Random.Range(-canvasSize.y + rectTransform.sizeDelta.y / 2, canvasSize.y - rectTransform.sizeDelta.y / 2);
+         targetPosition = new Vector2(x, y);
+     }*/
     private void SetNewTargetPosition()
     {
-        Vector2 canvasSize = new Vector2(canvasRectTransform.rect.width, canvasRectTransform.rect.height) / 2f;
+        RectTransform canvasObjectRectTransform = CanvasObject.GetComponent<RectTransform>();
+        Vector2 canvasSize = new Vector2(canvasObjectRectTransform.rect.width, canvasObjectRectTransform.rect.height) / 2f;
         float x = Random.Range(-canvasSize.x + rectTransform.sizeDelta.x / 2, canvasSize.x - rectTransform.sizeDelta.x / 2);
         float y = Random.Range(-canvasSize.y + rectTransform.sizeDelta.y / 2, canvasSize.y - rectTransform.sizeDelta.y / 2);
         targetPosition = new Vector2(x, y);
