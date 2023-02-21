cd059b815c851fd7be6837f6e8a8fae634533687
Amos <amos.koh.tk@gmail.com>
2023-02-21 12:17:34 +0000

    introduced new puzzle game
    simple drag and drop game

--- Assets/Scripts/Puzzle/DragandDrop.cs
+++ Assets/Scripts/Puzzle/DragandDrop.cs
@@ -1,67 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+
+
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragandDrop : MonoBehaviour
+public class DragandDrop : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    public GameObject correctForm;
-    private bool moving;
-    private bool finish;
+    public GameObject target;
 
-    private float startPosX;
-    private float startPosY;
+    private RectTransform sourceRectTransform;
+    private RectTransform targetRectTransform;
+    private Canvas canvas;
+    private Vector2 originalPosition;
 
-    private Vector3 resetPosition;
-
-    void Start()
+    private void Start()
     {
-        resetPosition = this.transform.localPosition;
+        sourceRectTransform = GetComponent<RectTransform>();
+        targetRectTransform = target.GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        originalPosition = sourceRectTransform.anchoredPosition;
     }
 
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        if (finish == false)
-        {
-            if (moving)
-            {
-                Vector3 mousePos;
-                mousePos = Input.mousePosition;
-                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
-                this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
-            }
-        }
+        // Move the source UI image along with the mouse
+        sourceRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
-    private void OnMouseDown()
+    public void OnEndDrag(PointerEventData eventData)
     {
-        if (Input.GetMouseButtonDown(0))
+        // If the source UI image is close enough to the target UI image, snap it to the target
+        if (Vector2.Distance(sourceRectTransform.anchoredPosition, targetRectTransform.anchoredPosition) < 50f)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
-            startPosX = mousePos.x - this.transform.localPosition.x;
-            startPosY = mousePos.y - this.transform.localPosition.y;
-
-            moving = true;
-        }
-    }
-
-    private void OnMouseUp()
-    {
-        moving = false;
-
-        
-        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f &&
-            Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
-        {
-            this.transform.position = new Vector3(correctForm.transform.position.x, correctForm.transform.position.y, correctForm.transform.localPosition.z);
-            finish = true;
+            sourceRectTransform.anchoredPosition = targetRectTransform.anchoredPosition;
         }
         else
         {
-            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
-        } 
+            // If the source UI image is not close enough to the target, reset its position to the original
+            sourceRectTransform.anchoredPosition = originalPosition;
+        }
     }
 }
+
--- Assets/Scripts/Quiz/QuestionGenerator.cs
+++ Assets/Scripts/Quiz/QuestionGenerator.cs
@@ -10,7 +10,7 @@ public class QuestionGenerator : MonoBehaviour
     public GameObject questionPanel;
     public static string actualAnswer;
     public static bool displayingQuestion = false;
-    private int questionNumber = 0;
+    private int questionNumber = 1;
  /*   public string NextScene;*/
     public Question[] questions;
     
@@ -29,16 +29,17 @@ public class QuestionGenerator : MonoBehaviour
     void Update()
     {
         //Will generate a question only if there is not a question being displayed
-        if (displayingQuestion == false && questionNumber <= totalQuestions)
+        if (displayingQuestion == false && questionNumber <= totalQuestions + 1)
         {
             displayingQuestion = true;
-            questionNumber++;
+            
             QuestionDisplay.newQuestion = questions[questionNumber - 1].questionText;
             QuestionDisplay.newA = questions[questionNumber - 1].optionA;
             QuestionDisplay.newB = questions[questionNumber - 1].optionB;
             QuestionDisplay.newC = questions[questionNumber - 1].optionC;
             QuestionDisplay.newD = questions[questionNumber - 1].optionD;
             actualAnswer = questions[questionNumber - 1].correctAnswer;
+            questionNumber++;
         }
         else if (questionNumber > totalQuestions)
         {
