656c46ff16b8a1fc866ca71e3fcb818b62c2e96b
Amos <amos.koh.tk@gmail.com>
2023-03-01 17:20:29 +0000

    updated bear scene

--- Assets/Scripts/Quiz/QuestionGenerator.cs
+++ Assets/Scripts/Quiz/QuestionGenerator.cs
@@ -10,7 +10,7 @@ public class QuestionGenerator : MonoBehaviour
     public GameObject questionPanel;
     public static string actualAnswer;
     public static bool displayingQuestion = false;
-    private int questionNumber = 1;
+    private int questionNumber = 0;
  /*   public string NextScene;*/
     public Question[] questions;
     
@@ -29,17 +29,17 @@ public class QuestionGenerator : MonoBehaviour
     void Update()
     {
         //Will generate a question only if there is not a question being displayed
-        if (displayingQuestion == false && questionNumber <= totalQuestions + 1)
+        if (displayingQuestion == false && questionNumber <= totalQuestions)
         {
             displayingQuestion = true;
-            
+            questionNumber++;
             QuestionDisplay.newQuestion = questions[questionNumber - 1].questionText;
             QuestionDisplay.newA = questions[questionNumber - 1].optionA;
             QuestionDisplay.newB = questions[questionNumber - 1].optionB;
             QuestionDisplay.newC = questions[questionNumber - 1].optionC;
             QuestionDisplay.newD = questions[questionNumber - 1].optionD;
             actualAnswer = questions[questionNumber - 1].correctAnswer;
-            questionNumber++;
+            
         }
         else if (questionNumber > totalQuestions)
         {
