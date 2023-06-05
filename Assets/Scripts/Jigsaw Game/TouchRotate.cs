27d8aa5c3ec18269f9283638fa7e77efa26ecd69
Tian <Tian.pan2003@gmail.com>
2023-06-05 07:04:22 +0100

    Dialogue changes and added sound effects

--- Assets/Scripts/Jigsaw Game/TouchRotate.cs
+++ Assets/Scripts/Jigsaw Game/TouchRotate.cs
@@ -5,9 +5,12 @@ using UnityEngine.EventSystems;
 
 public class TouchRotate : MonoBehaviour, IPointerClickHandler
 {
+    public AudioSource clickerFX;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         if (!GameControl.youWin)
         transform.Rotate(0, 0, 90);
+        clickerFX.Play();
     }
 }
