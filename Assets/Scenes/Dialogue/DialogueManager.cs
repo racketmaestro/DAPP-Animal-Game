ad488e8ddf215528363aceed58e6517893646903
Millie986 <Millie200302@outlook.com>
2023-06-05 18:22:29 +0800

    Dialogue_V1

---
+++ Assets/Scenes/Dialogue/DialogueManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    private static DialogueManager instance;
+    private Story currentStory;
+    private bool dialogueIsPlaying;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found more than one Dialogue Manager in the scene");
+        }
+        instance = this;
+    }
+
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+     //   if (InputManager.GetInstance().GetSubmitPressed())
+     //   {
+     //       ContinueStory();
+      //  }
+    }
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+       
+    }
+    private void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+
+        ContinueStory();
+    }
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+
+}
---
+++ Assets/Scenes/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+}
